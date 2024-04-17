c3a3b4b639b2cf7243aa3a58a0968286de49c22a
mtahir-cloudkitect <mtahir@cloudkitect.com>
2024-04-17 13:21:44 -0700

    feat(free-2): project structure for patterns

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,18 +3,10 @@ import { monorepo } from "@aws/pdk";
 import {NpmAccess} from "projen/lib/javascript";
 import {AwsCdkConstructLibrary} from "projen/lib/awscdk";
 
-const releaseBranch = 'main'
 const packagesDir = 'packages';
 const scope = '@cloudkitect';
 const packagesPath = `${packagesDir}/${scope}`;
-const cdkVersion = '2.137.0';
-const constructVersion = '10.3.0'
-const authorAddress = 'support@cloudkitect.com';
-const author = 'CloudKitect Inc'
-const repoUrl = 'https://github.com/cloudkitect/freemium';
-const jsiiVersion = '~5.3.0';
-const majorVersion = 1
-const pnpmVersion = "8"
+
 
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
@@ -25,24 +17,22 @@ const project = new monorepo.MonorepoTsProject({
   githubOptions: {
     mergify: false
   },
-  pnpmVersion: pnpmVersion,
+  pnpmVersion: "8",
   workspaceConfig: {
     linkLocalWorkspaceBins: true,
   },
 });
 
-const components = new AwsCdkConstructLibrary({
-  authorAddress: authorAddress,
+const subProjectSettings = {
+  authorAddress: 'support@cloudkitect.com',
   parent: project,
-  defaultReleaseBranch: releaseBranch,
-  cdkVersion: cdkVersion,
-  constructsVersion: constructVersion,
-  author: author,
+  defaultReleaseBranch: 'main',
+  cdkVersion: '2.137.0',
+  constructsVersion: '10.3.0',
+  author: 'CloudKitect Inc',
   authorOrganization: true,
-  repositoryUrl: repoUrl,
-  jsiiVersion: jsiiVersion,
-  name: `${scope}/components`,
-  outdir: `${packagesPath}/components`,
+  repositoryUrl: 'https://github.com/cloudkitect/freemium',
+  jsiiVersion: '~5.3.0',
   packageManager: project.package.packageManager,
   github: true,
   release: true,
@@ -50,11 +40,17 @@ const components = new AwsCdkConstructLibrary({
   githubOptions: {
     mergify: false,
   },
-  majorVersion: majorVersion,
+  majorVersion: 1,
   releasableCommits: ReleasableCommits.featuresAndFixes(),
-  pnpmVersion: pnpmVersion,
+  pnpmVersion: "8",
   npmAccess: NpmAccess.PUBLIC,
   npmProvenance: false,
+}
+
+const components = new AwsCdkConstructLibrary({
+  ...subProjectSettings,
+  name: `${scope}/components`,
+  outdir: `${packagesPath}/components`,
   keywords: ["cloudkitect", "cdk", "freemium", "infrastructure as code", "aws",
     "cloud components", "cdk constructs", "components", "terraform", "cloud compliance", "well architected"],
 });
@@ -62,6 +58,19 @@ components.synth()
 
 const releaseYaml = project.tryFindObjectFile('.github/workflows/release_cloudkitect-components.yml');
 releaseYaml!.addOverride('jobs.release_npm.steps.5.run', 'cd .repo && pnpm i --no-frozen-lockfile')
+
+const patterns = new AwsCdkConstructLibrary({
+  ...subProjectSettings,
+  name: `${scope}/patterns`,
+  outdir: `${packagesPath}/patterns`,
+  keywords: ["cloudkitect", "cdk", "freemium", "infrastructure as code", "aws",
+    "cloud components", "cdk constructs", "patterns", "terraform", "cloud compliance", "well architected"]
+});
+
+patterns.addDeps(`${scope}/components`)
+patterns.addPeerDeps(`${scope}/components`)
+patterns.addDevDeps(`${scope}/components`)
+patterns.synth();
 
 const exclusions = ['.DS_Store', '.idea', '*.iml']
 project.gitignore.exclude(...exclusions)
